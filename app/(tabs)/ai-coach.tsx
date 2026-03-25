cf0e02ea3887b19b2960e2f51ae4eb8200aa11dd
youness52 <anayouness52@gmail.com>
2026-03-25 01:29:50 +0100

    chore: update Gegmini API key.

--- app/(tabs)/ai-coach.tsx
+++ app/(tabs)/ai-coach.tsx
@@ -6,6 +6,12 @@ import { useWorkoutStore } from '@/store/workoutStore';
 import Colors from '@/constants/colors';
 import { generateWorkoutFromAI } from '@/services/aiService';
 
+
+export default function AiCoachScreen() {
+    const router = useRouter();
+    const { addCustomWorkout } = useWorkoutStore();
+    //const geminiApiKey = 'AIzaSyCW_BN7C2UeABRthg0Ekjm6s37ZM_T7JRw';
+
 const [geminiApiKey, setGeminiApiKey] = useState(null);
 
 React.useEffect(() => {
@@ -21,12 +27,6 @@ React.useEffect(() => {
 
     fetchKey();
 }, []);
-
-export default function AiCoachScreen() {
-    const router = useRouter();
-    const { addCustomWorkout } = useWorkoutStore();
-    //const geminiApiKey = 'AIzaSyCW_BN7C2UeABRthg0Ekjm6s37ZM_T7JRw';
-
 
     const [goals, setGoals] = useState('');
     const [experience, setExperience] = useState('');
